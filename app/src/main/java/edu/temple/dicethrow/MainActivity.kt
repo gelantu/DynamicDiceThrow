f5d9161121ad74473fe6b9f6448f6d16b514f85a
Gelan Balcha <tuo96713@temple.edu>
2024-10-30 16:28:22 -0400

    loaded fragment

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,11 +1,8 @@
 package edu.temple.dicethrow
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 
 /*
 Our DieThrow application has been refactored to move the dieRoll() logic
@@ -19,22 +16,35 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+    private var isLandscape = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
-        /* TODO 1: Load fragment(s)
-            - Show only Button Fragment if portrait
-            - show both fragments if Landscape
-          */
+        if (savedInstanceState == null) {
+            loadInitialFragments()
+        }
+    }
+    /* TODO 1: Load fragment(s)
+       - Show only Button Fragment if portrait
+       - show both fragments if Landscape
+     */
+
+    private fun loadInitialFragments() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container1, ButtonFragment())
+        if (isLandscape) {
+            transaction.replace(R.id.container2, DieFragment())
+        }
+        transaction.commit()
     }
 
     /* TODO 2: switch fragments if portrait (no need to switch fragments if Landscape)
         */
     // Remember to place Fragment transactions on BackStack so then can be reversed
+
     override fun buttonClicked() {
-
     }
-
-
 }
