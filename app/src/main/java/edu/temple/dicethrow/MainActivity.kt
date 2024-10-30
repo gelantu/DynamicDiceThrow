f3dc68fafcaf08464c067dcac59006126aca0e4a
Gelan Balcha <tuo96713@temple.edu>
2024-10-30 16:32:57 -0400

    switch fragment if portrait

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -46,5 +46,11 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     // Remember to place Fragment transactions on BackStack so then can be reversed
 
     override fun buttonClicked() {
+        if (!isLandscape) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, DieFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
